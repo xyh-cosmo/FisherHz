f553c43c7880c07bea52d86f6c6f79d7feafe3fb
xyh-cosmo <yhxu@nao.cas.cn>
2017-04-21 00:04:31 +0800

    debug ...

--- python/HzCosmoBase.py
+++ python/HzCosmoBase.py
@@ -2,15 +2,14 @@
 import sys,os
 import numpy as np
 from scipy.interpolate import interp1d
-from scipy.integrate import quad
-# import scipy as sp
+from scipy.integrate import quad, romberg
 import matplotlib.pylab as plt
 
 class HzCosmo:
 	"""
 	Simple cosmology module.  Interplolate over {zi,Ei} to compute luminosity distance.
 	"""
-	def __init__(self,z_interp=[],zmax=1.5,OmegaM=0.3,OmegaK=0.0,H0=70.0,cosmo_array_size=100,dL_zmin=0.005):
+	def __init__(self,z_interp=[],zmax=1.5,OmegaM=0.3,OmegaK=0.0,H0=70.0,cosmo_array_size=100,dL_zmin=0.01):
 		self.clight = 2.9979e5	# speed of light, in unit of km/s
 		self.Om	= OmegaM
 		self.Ok	= OmegaK
@@ -20,12 +19,12 @@ class HzCosmo:
 		self.rMB = 5.*np.log10(self.clight/self.H0) + 25.0 + self.MB # reduce absolute magnitude in B-band
 
 		self.interplnE = None
-		self.interldL = None
 		self.array_size = cosmo_array_size
 		self.dL_z = np.linspace(dL_zmin,zmax,self.array_size)
 		self.dL = np.zeros(self.array_size)
 		self.mB = np.zeros(self.array_size)
-		self.dmBdE = []  # store derivatives of mB wrt to Ei
+		self.dmBdE = []  # store derivatives of mB wrt to Ei & rMB
+		self.dmBdrMB = None
 
 		if len(z_interp) < 5:
 			print('length of z_interp is too small, pls increase it!')
@@ -36,6 +35,7 @@ class HzCosmo:
 			sys.exit(0)
 
 		z_size = len(z_interp)
+		self.z_interp = None
 		if min(z_interp) == 0.0:
 			z_size += 1
 			self.z_interp = np.array(z_interp)
@@ -47,7 +47,7 @@ class HzCosmo:
 			self.z_interp = np.array(ztemp)
 
 		def Efun(z):
-			return (self.Om*(1+z)**3 + self.Ol)**0.5
+			return (self.Om*(1+z)**3 + self.Ol +self.Ok*(1+z)**2)**0.5
 		self.E_fid = Efun(self.z_interp)
 		self.E_interp = self.E_fid
 		self.interplnE= interp1d(self.z_interp,np.log(self.E_interp))
@@ -64,32 +64,83 @@ class HzCosmo:
 		print('rMB    = %g'%self.rMB)
 
 	def UpdateEfun(self,E_idx,dE=0.01):
-		if E_idx >=1 and E_idx <= len(self.z_interp):
-			print '--> updating %2d -th Ei'
+		if E_idx >=1 and E_idx < len(self.z_interp):
+			print '--> updating %2d -th Ei'%(E_idx)
 			self.E_interp[E_idx] += dE
-			self.interplnE= interp1d(self.z_interp,log(self.E_interp))
-			self.E_interp[E_idx] = self.E_fid[E_idx] # reset to the fiducial value
+			self.interplnE= interp1d(self.z_interp,np.log(self.E_interp),kind='linear')
+
+		self.UpdateHzCosmo()
+		self.E_interp[E_idx] = self.E_fid[E_idx] # reset to the fiducial value
+
+
+	def ResetEfun(self,E_idx,dE=0.01):
+		self.E_interp[E_idx] = self.E_fid[E_idx] # reset to the fiducial value
 
 	def UpdateHzCosmo(self):
 		def Einv(z):
-			return 1./self.Efun(z)
+			return 1./np.exp(self.interplnE(z))
 
-		for i in range(len(self.dL_z)):
-			self.dL[i] = quad(Einv,0.,self.dL_z[i])[0]
+
+		self.dL[0] = romberg(Einv,0.,self.dL_z[0],divmax=50)
+		self.mB[0] = 5.0*np.log10((1.0+self.dL_z[0])*self.dL[0]) + self.rMB
+		for i in range(1,len(self.dL_z)):
+			# self.dL[i] = quad(Einv,0.,self.dL_z[i])[0]
+			self.dL[i] = self.dL[i-1] + romberg(Einv,self.dL_z[i-1],self.dL_z[i],divmax=50)
 			self.mB[i] = 5.0*np.log10((1.0+self.dL_z[i])*self.dL[i]) + self.rMB
 
 
 	def Efun(self,z):
 		return np.exp(self.interplnE(z))
 
+	def ComputedmB(self):
+		# compute dmB/dEi
+		dE = 0.01
+		for i in range(1,len(self.z_interp)):
+			UppermB = []
+			LowermB = []
+			self.UpdateEfun(i,dE)
+			for j in range(len(self.mB)):
+				UppermB.append(self.mB[j])
+			UppermB = np.array(UppermB)
+			# print 'UppermB = ',UppermB[0:10]
+
+			self.UpdateEfun(i,-1*dE)
+			for j in range(len(self.mB)):
+				LowermB.append(self.mB[j])
+			# print 'UppermB = ',UppermB[0:10]
+			# print 'LowermB = ',LowermB[0:10]
+			dmB = 0.5*(UppermB-LowermB)/dE
+			# print 'dmB = ',dmB
+			# for j in range(len(UppermB)):
+				# print 'UppermB - LowermB = %10.5e'%(UppermB[j]-LowermB[j])
+			# sys.exit(0)
+			self.dmBdE.append(interp1d(self.dL_z,dmB))
+
+		# compute dmB/drMB
+		self.dmBdrMB = np.ones(self.array_size)
+
 	def run_test(self):
-		plt.subplot(1,2,1)
+		# zxx = np.linspace(0.,max(self.z_interp),20)
+		# plt.plot(self.z_interp,np.log(self.E_interp),'-o')
+		# plt.plot(zxx,self.interplnE(zxx),'-s')
+
+		plt.figure(figsize=(10,8))
+
+		plt.subplot(2,2,1)
 		plt.plot(self.z_interp,self.E_fid,'-ro',label=r'test E(z)')
 		plt.legend()
 
-		plt.subplot(1,2,2)
+		plt.subplot(2,2,2)
 		plt.plot(self.dL_z,self.mB,label=r'apparent magnitude')
 		plt.legend()
+
+		plt.subplot(2,2,3)
+		for i in range(len(self.z_interp)-1):
+			print 'I = ',i
+			plt.plot(self.dL_z,self.dmBdE[i](self.dL_z),label=r'E'+str(i+1))
+		plt.legend(ncol=3)
+
+
 
 		plt.show()
 
@@ -98,7 +149,8 @@ class HzCosmo:
 
 # TEST part
 
-z_interp = [0.1,0.3,0.5,0.7,0.9,1.1,1.3,1.5]
-HC = HzCosmo(z_interp=z_interp)
+z_interp = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1.0,1.1,1.2,1.3,1.4,1.5]
+HC = HzCosmo(z_interp=z_interp,cosmo_array_size=100)
+HC.ComputedmB()
 
 HC.run_test()
